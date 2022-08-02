0204987b2660159f2d9990b76a1474c2e0e13d1e
julfiker755 <julfiker755.bd@gmail.com>
2022-08-02 11:45:12 +0600

    first commit

---
+++ src/Component/CustomLink/CustomLink.js
@@ -0,0 +1,19 @@
+import {useMatch,useResolvedPath,Link} from 'react-router-dom'
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+  
+    return (
+      <div>
+        <Link
+          style={{color:match ? 'green':'black', textDecoration: match ? "underline" : "none" }}
+          to={to}
+          {...props}
+        >
+          {children}
+        </Link>
+      </div>
+    );
+  }
+
+  export default CustomLink;
---
+++ src/Component/Servies/Servies.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Servies() {
+    return (
+        <div>
+             <h1 className='text-[40px] text-[red]'>Hello I am Servies Component</h1>
+        </div>
+    )
+}
+
+export default Servies
